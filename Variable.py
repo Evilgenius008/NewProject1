de405b816320bd6c42d47c079d0d13688c7184f3
Evilgenius008 <lovygin976@gmail.com>
2024-11-05 19:50:00 +0300

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,8 @@
+number_of_dz = "number_of_dz"
+dz=12
+hour="hour"
+hr=1.5
+course_name = "Python,"
+Time_for_one_task = "Time_for_one_task"
+average_execution_time = "average_execution_time:"
+print(course_name,number_of_dz,dz,hour,hr,average_execution_time,hr/dz,hour)
